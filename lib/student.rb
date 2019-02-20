ee47ada159e4b322a1c1eaa40419dfedf7f1d862
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-20 23:40:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,11 +68,11 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE name = ?
+      WHERE grade = 9
       LIMIT 1
     SQL
  
-    DB[:conn].execute(sql, name).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end.first
   end
