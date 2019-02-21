2ee5986e8db91e90ca980c0490b4ab0bd48efeb4
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-21 00:02:32 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -102,7 +102,15 @@ class Student
   end 
   
   def self.first_student_in_grade_10
-    
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade < 12 
+    SQL
+ 
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
 end
