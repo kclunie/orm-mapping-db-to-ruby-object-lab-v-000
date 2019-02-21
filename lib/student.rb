ba5a63015505da56d026a7eaed1868a809d72852
Kristen Clunie <kcluniesa@yahoo.com>
2019-02-21 00:01:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,10 +90,10 @@ class Student
   
   def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
-      SELECT ?
+      SELECT *
       FROM students
       WHERE grade = 10
-      ORDER BY id 
+      LIMIT ?
     SQL
  
     DB[:conn].execute(sql, x).map do |row|
